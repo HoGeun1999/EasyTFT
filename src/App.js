12aead8b83b3a050ebb21f14c217c213920eb80b
Ho <cgh100039@gmail.com>
2025-01-07 18:20:48 +0900

    app.js fetch function change

--- src/App.js
+++ src/App.js
@@ -1,70 +1,85 @@
 import './App.css';
 import ChampionBox from './component/ChampionBox/ChampionBox';
-import ItemBox from './/component/ItemBox/ItemBox'
+import ItemBox from './component/ItemBox/ItemBox';
 import TraitBox from './component/TraitBox/TraitBox';
 import RecommendDeckBox from './component/RecommendDeckBox/RecommendDeckBox';
 import SetChampionBox from './component/SetChampionBox/SetChampionBox';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [SetChampionBoxList, SettingChampionBoxList] = useState([])
+  const [SetChampionBoxList, SettingChampionBoxList] = useState([]);
   const [championList, setChampionList] = useState([]);  // 사용자 상태를 관리
   const [traitsData, setTraitsData] = useState(null); // 데이터를 저장할 상태 변수
   
   useEffect(() => {
-    fetch('https://hogeun1999.github.io/EasyTFT/TFTchampionData.json')
-      .then((response) => response.json())  // JSON으로 변환
-      .then((data) => {
+    // 비동기 함수 정의
+    const fetchChampionData = async () => {
+      try {
+        const response = await fetch('/TFTchampionData.json');
+        const data = await response.json();  // JSON으로 변환
         const relocatedData = [...data].sort((a, b) => {
           return a.name.localeCompare(b.name, 'ko');
         });
         setChampionList(relocatedData);
-      })
-      .catch((error) => {
+      } catch (error) {
         console.error('Error fetching data:', error);
-      });
-  }, []); 
+      }
+    };
+
+    fetchChampionData();  // 비동기 함수 호출
+  }, []);  // 컴포넌트가 처음 렌더링될 때만 실행
 
   useEffect(() => {
-    // public 폴더에 있는 TFTtraitsData.json 파일을 가져옵니다.
-    fetch('/TFTtraitsData.json')
-      .then((response) => response.json()) // JSON으로 파싱
-      .then((data) => setTraitsData(data))  // 데이터를 상태에 저장
-      .catch((error) => console.error("데이터를 가져오는 데 오류가 발생했습니다.", error)); // 에러 처리
-  }, []); // 컴포넌트가 처음 렌더링될 때만 실행
+    // 비동기 함수 정의
+    const fetchTraitsData = async () => {
+      try {
+        const response = await fetch('/TFTtraitsData.json');
+        const data = await response.json();  // JSON으로 파싱
+        setTraitsData(data);  // 데이터를 상태에 저장
+      } catch (error) {
+        console.error("데이터를 가져오는 데 오류가 발생했습니다.", error);
+      }
+    };
 
+    fetchTraitsData();  // 비동기 함수 호출
+  }, []);  // 컴포넌트가 처음 렌더링될 때만 실행
 
   return (
     <div className="App">
       <div className="grid-container">
         <div className="grid-item-ChampionBox">
           <ChampionBox 
-          championList={championList} 
-          setChampionList={setChampionList} 
-          SetChampionBoxList={SetChampionBoxList} 
-          SettingChampionBoxList={SettingChampionBoxList}/>
+            championList={championList} 
+            setChampionList={setChampionList} 
+            SetChampionBoxList={SetChampionBoxList} 
+            SettingChampionBoxList={SettingChampionBoxList}
+          />
         </div>
         <div className="grid-item-ItemBox">
           <ItemBox />
         </div>
         <div className="grid-item-TraitBox">
           <TraitBox 
-          SetChampionBoxList={SetChampionBoxList}
-          traitsData={traitsData}/>
+            SetChampionBoxList={SetChampionBoxList}
+            traitsData={traitsData}
+          />
         </div>
         <div className="grid-item-RecommendDeckBox">
           <RecommendDeckBox 
-          SetChampionBoxList={SetChampionBoxList} 
-          SettingChampionBoxList={SettingChampionBoxList}/>
+            SetChampionBoxList={SetChampionBoxList} 
+            SettingChampionBoxList={SettingChampionBoxList}
+          />
         </div>
         <div className="grid-item-SetChampionBox">
           <SetChampionBox 
-          SetChampionBoxList={SetChampionBoxList} 
-          SettingChampionBoxList={SettingChampionBoxList} />
+            SetChampionBoxList={SetChampionBoxList} 
+            SettingChampionBoxList={SettingChampionBoxList}
+          />
         </div>
       </div>
       <div className='version'>14.24 version</div>
     </div>
   );
 }
+
 export default App;
